f45de1fad74ac293f1fba271b36a0980965e95c8
Serg111-alt <56454209+Serg111-alt@users.noreply.github.com>
2020-06-12 10:43:07 +0300

    Update Model.php

--- model/Model.php
+++ model/Model.php
@@ -40,11 +40,23 @@ class Model {
           <title>{$this->name}</title>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1">
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-          <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-          <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+                 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	  <script type="text/javascript" src="js/scripts.js"></script>
+    <meta name="description" content="This tiny jQuery Bootstrap WYSIWYG plugin turns any DIV into a HTML5 rich text editor" />
+    <link rel="apple-touch-icon" href="//mindmup.s3.amazonaws.com/lib/img/apple-touch-icon.png" />
+    <link rel="shortcut icon" href="favicon.ico" >
+    <link href="external/google-code-prettify/prettify.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="external/jquery.hotkeys.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+    <script src="external/google-code-prettify/prettify.js"></script>
+		
+    <script src="bootstrap-wysiwyg.js"></script>
+    <script src="script.js"></script>
           <style>
           body {
               font: 400 15px Lato, sans-serif;
